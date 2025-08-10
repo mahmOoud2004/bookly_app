a17cebe5b9a9be04135c08ef1cf665e51dba0cfa
MahmoudMM <mans72749@gmail.com>
2025-08-10 21:52:53 +0300

    home list view

--- lib/features/splash/precention/viwes/widgets/splash_view_body.dart
+++ lib/features/splash/precention/viwes/widgets/splash_view_body.dart
@@ -1,9 +1,72 @@
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with TickerProviderStateMixin {
+  late AnimationController _logoController;
+  late Animation<double> _logoScale;
+  late Animation<double> _logoOpacity;
+
+  late AnimationController _textController;
+  late Animation<double> _textOpacity;
+  late Animation<Offset> _textSlide;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Logo animation controller
+    _logoController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1200),
+    );
+
+    _logoScale = Tween<double>(begin: 0.8, end: 1.0).animate(
+      CurvedAnimation(parent: _logoController, curve: Curves.easeOutBack),
+    );
+
+    _logoOpacity = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(parent: _logoController, curve: Curves.easeIn));
+
+    // Text animation controller
+    _textController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    );
+
+    _textOpacity = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(parent: _textController, curve: Curves.easeIn));
+
+    _textSlide = Tween<Offset>(
+      begin: const Offset(0, 0.5),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: _textController, curve: Curves.easeOut));
+
+    // Start animations with delay
+    _logoController.forward().then((_) {
+      Future.delayed(const Duration(milliseconds: 200), () {
+        _textController.forward();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _logoController.dispose();
+    _textController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,15 +74,39 @@ class SplashViewBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Center(child: Image.asset(AssetsData.logo)),
-        SizedBox(height: 16),
-        Text(
-          "read free books",
-          textAlign: TextAlign.center,
-          style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-            fontSize: 20,
-            fontWeight: FontWeight.w600,
+        Center(
+          child: AnimatedBuilder(
+            animation: _logoController,
+            builder: (context, child) {
+              return Opacity(
+                opacity: _logoOpacity.value,
+                child: Transform.scale(
+                  scale: _logoScale.value,
+                  child: Image.asset(AssetsData.logo),
+                ),
+              );
+            },
           ),
+        ),
+        const SizedBox(height: 16),
+        AnimatedBuilder(
+          animation: _textController,
+          builder: (context, child) {
+            return Opacity(
+              opacity: _textOpacity.value,
+              child: SlideTransition(
+                position: _textSlide,
+                child: Text(
+                  "read free books",
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            );
+          },
         ),
       ],
     );
