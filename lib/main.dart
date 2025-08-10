e4d42122cb840adc4610dbce72cf818b6f301214
MahmoudMM <mans72749@gmail.com>
2025-08-10 10:49:37 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: const MyHomePage(title: 'Flutter Demo Home Page'));
+  }
+}
